d8920dc146d973a6bd9121af3d82a6183b083559
Shovonx47 <shovon@lamptechs.com>
2025-12-30 00:57:10 +0600

    Coverage Plot Module

---
+++ src/app/coverage-plot/new-form/page.js
@@ -0,0 +1,11 @@
+import { CreateCoveragePlotForm } from "./create-coverage-plot-form"
+
+export default function CreateCoveragePlotPage() {
+    return (
+        <div className="p-8 w-full max-w-[1800px] mx-auto">
+            <CreateCoveragePlotForm />
+        </div>
+    )
+}
+
+
